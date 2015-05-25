bfaf26218904d54c27c59a27f4cb03240eee94cc
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-05-25 22:29:53 +0200

    Update eater.scm

--- eater.scm
+++ eater.scm
@@ -18,3 +18,24 @@
 #{procedure 9013 (eat in state-loop in eater)}
 41> ((((eater 1) 2) 3) 'vomit)
 (1 2 3)
+
+;; we can achieve the same effect by replacing 'eat' with a unnamed lambda function
+
+(define (munch x)
+  (define (loop state)
+    (lambda (x) 
+      (if (eq? x 'vomit)
+        (reverse state)
+        (loop (cons x state)))))
+  ((loop '()) x))
+
+41> munch
+#{procedure 9049 munch}
+41> (munch 1)
+#{procedure 9051 (unnamed in loop in munch)}
+41> ((munch 1) 2)
+#{procedure 9051 (unnamed in loop in munch)}
+41> (((munch 1) 2) 3)
+#{procedure 9051 (unnamed in loop in munch)}
+41> ((((munch 1) 2) 3) 'vomit)
+(1 2 3)
